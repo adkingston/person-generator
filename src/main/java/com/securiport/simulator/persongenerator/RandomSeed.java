8fac39fb680f73c72e25f4e5485f947ca37028b7
Alexander <Alexander@DESKTOP-7SK1SGU>
2017-07-24 07:52:06 -0400

    Simplified random number generator

--- src/main/java/com/securiport/simulator/persongenerator/RandomSeed.java
+++ src/main/java/com/securiport/simulator/persongenerator/RandomSeed.java
@@ -2,24 +2,25 @@ package com.securiport.simulator.persongenerator;
 
 import java.util.Random;
 
-public class RandomSeed extends BaseRandomGenerator implements RandomGenerator {
+public class RandomSeed {
 	
 
 
-	public int getNum(int seed) {
-		Random randNum = new Random(seed);
-		int numRows = 1000; // 1000 rows in random_name.csv
-		int r = randNum.nextInt(numRows); // 13514 used for now - current number of rows in all csv files. 
+	private Random _randNum;
+	
+	public RandomSeed () {
+		Long M = System.currentTimeMillis();
+		String F = Long.toString(M);
+		int S = Integer.parseInt(F.substring(F.length()-10));
+		_randNum = new Random(S);
+	}
+
+	public RandomSeed(int S) {
+		_randNum = new Random(S);
+	}
+	
+	public int getNum(int maxRows) {
+		int r = _randNum.nextInt(maxRows); // 13514 used for now - current number of rows in all csv files. 
 		return r;	
-	}
-	
-	public int showSeed() {
-		return this.seed;
-	}
-	
-	@Override
-	public int nextNum() {
-		this.seed++;
-		return getNum(this.seed);
 	}
 }
--- src/test/java/com/securiport/simulator/persongenerator/RandomGeneratorTest.java
+++ src/test/java/com/securiport/simulator/persongenerator/RandomGeneratorTest.java
@@ -15,43 +15,36 @@ public class RandomGeneratorTest {
 	@Before 
 	public void beforeTest() {
 		_seed = 0;
-		_rs = new RandomSeed();
-		_r1 = new RandomSeed();
-		_r2 = new RandomSeed();
+		_rs = new RandomSeed(0);
+		_r1 = new RandomSeed(1);
+		_r2 = new RandomSeed(1);
 	}
 	
-	@Test
-	public void initializationTest() {
-		try {
-			_rs.initialize(_seed);
-		} catch ( Exception ex ) {
-			fail(ex.getMessage());
-		}
-	}
-	
-	@Test 
-	public void observeZeroInitializationTest() {
-		// given a zero initialization parameter, will a new seed be assigned as necessary?
-		_rs.initialize(_seed);
-		assertTrue(_rs.showSeed() != 0);
-	}
+//	@Test
+//	public void initializationTest() {
+//		try {
+//			_rs.initialize(_seed);
+//		} catch ( Exception ex ) {
+//			fail(ex.getMessage());
+//		}
+//	}
 	
 	int S = 659810410; // test seed 
 	
 	@Test
 	public void seededResults() {
 		// test to see if number lists are random, and if same seed results in same list of numbers
- 
-		_r1.initialize(S);
-		_r2.initialize(S);
+
 		int iterations = 300000;
 		int n1;
 		int n2;
 		for (int i=0; i<iterations; i++) {
-			n1 = _r1.nextNum();
-			n2 = _r2.nextNum();
+			n1 = _r1.getNum(1000);
+			n2 = _r2.getNum(1000);
 			System.out.println(n1);
 			assertTrue(n1 == n2);
 		}
 	}
+	
+	
 }
